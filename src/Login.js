13a5c44d698b86b2593dd4eb9e478086923b9c9e
Prabhudev S Betsurmath <prabhudevbetsurmath@gmail.com>
2020-11-17 23:19:21 +0530

    logo and images chnaged

--- src/Login.js
+++ src/Login.js
@@ -38,7 +38,7 @@ function Login() {
             <Link to='/'>
                 <img
                     className="login__logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+                    src='https://rcdiamonds.com/skin/frontend/sphere/default/images/logo.png' 
                 />
             </Link>
 
